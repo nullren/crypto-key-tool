d2accfb2b5ea5fd1eb0c7984e6114b3e515ac857
Renning Bruns <ren@renmail.net>
2024-11-04 21:26:44 -0800

    print all types from private key hex

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,41 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    println!("Private key: {}", args.private_key);
 
     let private_key = parse_wif(&args.private_key)?;
-    let public_address = private_key.to_public_address()?;
-    println!("Public address: {}", public_address);
+    println!("Private key: {}", hex::encode(private_key.private_key_bytes()));
+    
+    for compressed in vec![true, false].iter() {
+        for network in vec![Network::Mainnet, Network::Testnet].iter() {
+            let wif = private_wif_key(&private_key.private_key_bytes(), &network, *compressed);
+            println!("Private compressed {} network {}: {}", compressed, network, wif);
+
+            let public_address = private_key.to_public_address(*compressed, &network)?;
+            println!("Public compressed {} network {}: {}", compressed, network, public_address);
+        }
+    }
+    
     Ok(())
 }
 
 #[derive(Debug, Clone)]
 struct PrivateKey {
-    network: Network,
-    compressed: bool,
     secret_key: SecretKey,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, clap::ValueEnum)]
 enum Network {
     Mainnet,
     Testnet,
+}
+
+impl std::fmt::Display for Network {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Network::Mainnet => write!(f, "mainnet"),
+            Network::Testnet => write!(f, "testnet"),
+        }
+    }
 }
 
 impl TryFrom<u8> for Network {
@@ -49,36 +65,42 @@ impl TryFrom<u8> for Network {
     }
 }
 
-impl From<Network> for u8 {
-    fn from(network: Network) -> u8 {
-        match network {
+impl Network {
+    fn public_byte(&self) -> u8 {
+        match self {
             Network::Mainnet => 0x00,
             Network::Testnet => 0x6f,
+        }
+    }
+    fn private_byte(&self) -> u8 {
+        match self {
+            Network::Mainnet => 0x80,
+            Network::Testnet => 0xef,
         }
     }
 }
 
 impl PrivateKey {
     fn from_slice(
-        network: Network,
-        compressed: bool,
         bytes: &[u8],
     ) -> Result<Self, Box<dyn Error>> {
         let secret_key = SecretKey::from_slice(bytes)?;
         Ok(PrivateKey {
-            network,
-            compressed,
             secret_key,
         })
     }
 
-    fn public_key_bytes(&self) -> Vec<u8> {
-        public_key(&self.secret_key, self.compressed)
+    fn public_key_bytes(&self, compressed: bool) -> Vec<u8> {
+        public_key(&self.secret_key, compressed)
+    }
+    
+    fn private_key_bytes(&self) -> Vec<u8> {
+        self.secret_key.to_bytes().to_vec()
     }
 
-    fn to_public_address(&self) -> Result<String, Box<dyn Error>> {
-        let public_key = self.public_key_bytes();
-        Ok(public_address(&public_key, self.network.clone()))
+    fn to_public_address(&self, compressed: bool, network: &Network) -> Result<String, Box<dyn Error>> {
+        let public_key = self.public_key_bytes(compressed);
+        Ok(public_address(&public_key, network))
     }
 }
 
@@ -92,15 +114,13 @@ fn parse_wif(wif: &str) -> Result<PrivateKey, Box<dyn Error>> {
             if !(37..=38).contains(&len) {
                 return Err("Invalid WIF length".into());
             }
-            let compressed = len == 38;
 
             if !verify_checksum(bytes) {
                 return Err("Invalid checksum".into());
             }
 
-            let network = bytes[0].try_into()?;
             let private_key = &bytes[1..33];
-            PrivateKey::from_slice(network, compressed, private_key)
+            PrivateKey::from_slice(private_key)
         }
     }
 }
@@ -121,14 +141,25 @@ fn public_key(secret_key: &SecretKey, compressed: bool) -> Vec<u8> {
     public_key.to_encoded_point(compressed).as_bytes().to_vec()
 }
 
-fn public_address(public_key: &[u8], network: Network) -> String {
+fn public_address(public_key: &[u8], network: &Network) -> String {
     let sha256_hash = Sha256::digest(public_key);
     let ripemd_hash = Ripemd160::digest(sha256_hash);
-    let mut address_bytes = vec![network.into()];
+    let mut address_bytes = vec![network.public_byte()];
     address_bytes.extend(&ripemd_hash);
     let checksum = make_checksum(&address_bytes);
     address_bytes.extend(checksum);
     address_bytes.to_base58()
+}
+
+fn private_wif_key(private_key: &[u8], network: &Network, compressed: bool) -> String {
+    let mut wif_bytes = vec![network.private_byte()];
+    wif_bytes.extend(private_key);
+    if compressed {
+        wif_bytes.push(0x01);
+    }
+    let checksum = make_checksum(&wif_bytes);
+    wif_bytes.extend(checksum);
+    wif_bytes.to_base58()
 }
 
 #[cfg(test)]
@@ -136,27 +167,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn private_key_from_wif() {
-        let wif = "5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ";
-        let result = parse_wif(wif);
-        assert!(result.is_ok());
-        let private_key = result.unwrap();
-        assert_eq!(private_key.network, Network::Mainnet);
-        assert_eq!(private_key.compressed, false);
-    }
-
-    #[test]
     // key from https://www.freecodecamp.org/news/how-to-create-a-bitcoin-wallet-address-from-a-private-key-eca3ddd9c05f/
     fn test_convert_to_public_address() {
         let pk = PrivateKey::from_slice(
-            Network::Mainnet,
-            true,
             &hex::decode("60cf347dbc59d31c1358c8e5cf5e45b822ab85b79cb32a9f3d98184779a9efc2")
                 .unwrap(),
         )
         .unwrap();
 
-        let public_addr = pk.to_public_address();
+        let public_addr = pk.to_public_address(true, &Network::Mainnet);
         assert!(public_addr.is_ok());
 
         let public_addr = public_addr.unwrap();
@@ -170,7 +189,7 @@ mod tests {
         assert!(pk.is_ok());
         let pk = pk.unwrap();
 
-        let public_addr = pk.to_public_address();
+        let public_addr = pk.to_public_address(true, &Network::Mainnet);
         assert!(public_addr.is_ok());
 
         let public_addr = public_addr.unwrap();
